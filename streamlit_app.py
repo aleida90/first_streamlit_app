5ac7acb7f425e5b22da7b21f2fb744f08c0d0ab0
aleida90 <42223986+aleida90@users.noreply.github.com>
2022-08-15 08:12:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,26 @@
 import streamlit
 import snowflake.connector
+import pandas
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title(('Zena\'s Amazing Athleisure Catalog')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+                
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+
+ #put the data into a data frame
+df=pandas.DataFrame(my_catalog)
+# temp write the dataframe to the page so I can see what I am working with
+# streamlit.write(df)
+                
+ #put the first column into a list
+ color_list=df[0].values.tolist()
+ #print(color_list)
+                
+ #Let's put a pick list here so they can pick the color
+  option=streamlit.selectbox('Pick a sweatsuit color or style: ',list(color_list))
+ #We'll build the image caption now, since we can 
+                
+ product caption='Our warm, comfortable, '+option+' sweatsuit!'
